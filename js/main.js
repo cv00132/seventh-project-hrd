ea296c3fbdf50f5ab4bca59e688981e1b362be09
Cory Vaughan <codercory13@gmail.com>
2017-02-08 19:39:51 -0500

    Got carousel functional, but need to style.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,36 @@
-$("div").addClass("carousel");
+$(".carousel").html(`<a href="#" class="left-arrow">LeftArrow<span></span></a>
+`)
+$(".carousel").html(`<a href="#" class="right-arrow">RightArrow<span></span></a>
+`)
+$("#previous-img").addClass("hidden");
+$("#current-img").addClass("visible");
+var count = 0;
+
+var imageArray = $(carouselImages.map(function (x) {
+  return x.image_url;
+}));
+
+function updateBG () {
+  $(".carousel").attr('style', `background-image: url(${imageArray[count]})`)
+}
+updateBG();
+
+$(".right-arrow").click("click", rightClick)
+
+function rightClick (event) {
+  if (count >= 4) {
+    return count = 0
+  }
+  count += 1;
+  updateBG();
+}
+
+$(".left-arrow").click("click", leftClick)
+
+function leftClick (event) {
+  if (count <= 0) {
+    return count = 5;
+  }
+  count += 1;
+  updateBG();
+}
